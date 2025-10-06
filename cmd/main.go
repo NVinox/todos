637bfc899612337e0b21b07ff3de6e4710605189
Sergey klimov <sergey.k@pyrobyte.ru>
2025-10-06 15:53:29 +0700

    Добавил докер

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/NVinox/todos/internal/handler"
 	"github.com/NVinox/todos/internal/repository"
 	"github.com/NVinox/todos/internal/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatal("Could not read config file: %s", err.Error())
+	}
+
 	repository := repository.NewRepository()
 	service := service.NewService(repository)
 	handler := handler.NewHandler(service)
 
 	server := new(todos.Server)
-	if err := server.Run("3000", handler.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatal("Could not to run server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
