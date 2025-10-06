c9afc25723a75cd33e9dfedba2ed956d75816b57
Sergey klimov <sergey.k@pyrobyte.ru>
2025-10-06 12:35:27 +0700

    Описал функцию запуска сервера

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/NVinox/todos"
+)
+
+func main() {
+	server := new(todos.Server)
+
+	if err := server.Run("3000"); err != nil {
+		log.Fatal("Could not to run server: %s", err.Error())
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NVinox/todos
+
+go 1.24.2
